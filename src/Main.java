b312328d72c63101f95b0a23b060871f6fa0deb6
Василий Шкурихин <virtualshkus@yandex.ru>
2022-05-25 23:12:41 +0500

    change int priceTicket and delete folder out

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int priceBilet = 10_000; // цена авиабилета, рублей
+        int priceTicket = 10_000; // цена авиабилета, рублей
         int costMile = 20; // стоимость одной мили, рублей
-        int bonusMile = priceBilet / costMile; //расчет количества бонусных милей
+        int bonusMile = priceTicket / costMile; //расчет количества бонусных милей
 
         System.out.println("Количество милллей: " + bonusMile);
 
